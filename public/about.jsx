4c3e326681ee5152d8a1611bfbd37c258003615f
Syed Talha <syedtalha.thetechlabs@gmail.com>
2026-08-25 17:35:17 +0500

    about page added

---
+++ public/about.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class about extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This is the about page</h1>
+      </div>
+    )
+  }
+}
+
+export default about
